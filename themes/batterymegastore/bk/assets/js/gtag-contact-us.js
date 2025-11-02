86680a5540c257625b11a66ef64fd91fa84e13cf
ecomms-bms <ecomms@bmstechnologies.co.uk>
2025-11-02 18:02:45 +0000

    Delete themes/batterymegastore/bk directory

--- themes/batterymegastore/bk/assets/js/gtag-contact-us.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener('load', function() {
-    if (document.location.href.includes('/contact-us/')) {
-      gtag('event', 'conversion', {
-        'send_to': 'AW-600885221/ZUd_CKqh0IcYEOWPw54C'
-      });
-    }
-});
